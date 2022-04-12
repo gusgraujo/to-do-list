3b8c239624e3dc133fb89b7a8289ae21ec8e3aee
gusgraujo <gusg.raujo@gmail.com>
2022-04-12 00:01:35 -0300

    feature: Incluir paginas do EJS no projeto

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const checklistRouter = require('./src/routes/checklist')
-const app = express();
+const rootRouter = require('./src/routes/index')
+
+const path = require('path');
+
 require('./config/database')
+
+
+const app = express();
+app.use(express.json());
+
+app.set('views',path.join(__dirname,'src/views'));
+app.set('view engine','ejs');
+
+app.use('/checklists',checklistRouter);
+app.use('/',rootRouter);
+
+
 
 app.listen((3000), ()=>{
     console.log("Servidor iniciado!");
-});
-
-app.use(express.json());
-
-app.use('/checklists',checklistRouter);
-
-
-app.get('/json',(req,res) => {
-    console.log(req.body)
-    res.json({tittle:'Tarefa X', done :true});
 });
 
 
